d75de76344322b55c4b7521ec8b1971ddf359329
Lartg <roc.gerrety@gmail.com>
2021-10-28 17:00:52 -0800

    display playlists

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import render_template
 
 app = Flask(__name__)
 
+playlists = [
+  {'title': 'Chess Videos', 'description': 'Top games from TCEC'},
+  {'title': 'chill vibes', 'description': 'Resonance'}
+]
+
 @app.route('/')
-def index():
-  return render_template('home.html', msg='Flask is kinda cool I guess')
+def playlist_index():
+  """Show all playlists"""
+  return render_template('playlists_index.html', playlists = playlists)
 
 if __name__ == "__main__":
   app.run(debug=True)
