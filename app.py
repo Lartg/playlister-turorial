b31982170d8dbf81e5a97bde7f75fe2e356bda40
Lartg <roc.gerrety@gmail.com>
2021-10-28 22:38:02 -0800

    can add playlists

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ db = client.Playlister
 playlists = db.playlists
 
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -19,6 +18,19 @@ playlists = [
 def playlist_index():
   """Show all playlists"""
   return render_template('playlists_index.html', playlists = playlists)
+
+@app.route('/playlists/new')
+def playlists_new():
+  return render_template('playlists_new.html')
+
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+  playlist = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description')
+  }
+  playlists.append(playlist)
+  return redirect(url_for('playlist_index'))
 
 if __name__ == "__main__":
   app.run(debug=True)
