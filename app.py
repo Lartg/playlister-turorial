96b992b83f6ba2a9882e10f46bbaf9c1b403c695
Lartg <roc.gerrety@gmail.com>
2021-10-30 20:55:36 -0800

    I broke everything

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
 client = MongoClient()
 db = client.Playlister
 playlists = db.playlists
@@ -13,6 +13,12 @@ playlists = [
   {'title': 'Chess Videos', 'description': 'Top games from TCEC'},
   {'title': 'chill vibes', 'description': 'Resonance'}
 ]
+def video_url_creator(id_list):
+  videos = []
+  for vid_id in id_list:
+    video = 'https://youtube.com/embed/' + vid_id
+    videos.append(video)
+  return videos
 
 @app.route('/')
 def playlist_index():
@@ -25,12 +31,20 @@ def playlists_new():
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
+  video_ids =  request.form.get('video_ids').split()
+  videos = video_url_creator(video_ids)
   playlist = {
     'title': request.form.get('title'),
-    'description': request.form.get('description')
+    'description': request.form.get('description'),
+    'videos': videos,
+    'video_ids': video_ids
   }
   playlists.append(playlist)
   return redirect(url_for('playlist_index'))
+
+@app.route('/playlists/<playlist_id>')
+def display_playlist(playlist_id):
+  return print(f"my ID is: {playlist_id}")
 
 if __name__ == "__main__":
   app.run(debug=True)
