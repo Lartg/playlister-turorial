5c1abbbb7a0d4fca0846f8012279ddc80b3ecc6c
Lartg <roc.gerrety@gmail.com>
2021-10-31 20:42:16 -0800

    changed db location

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+
 client = MongoClient('mongodb+srv://Admin:kHwGiTilGkc8OEq4@cluster0.anqw0.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.Playlister
+db = client.get_default_database()
 playlists = db.playlists
 
 
